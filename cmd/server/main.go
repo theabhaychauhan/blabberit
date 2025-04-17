206be1a1aa5e01641cc73e9d49c93057f28aef4e
Abhay Chauhan <ch.abhay@gmail.com>
2025-04-17 18:14:11 +0530

    Starting to BlabberIt

---
+++ cmd/server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "BlabberIt server is running 🚀")
+	})
+
+	fmt.Println("Starting BlabberIt on :8080...")
+	http.ListenAndServe(":8080", nil)
+}
---
+++ internal/user/store.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func InitDB() {
+	_ = godotenv.Load(".env")
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASS"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Failed to connect to database:", err)
+	}
+
+	if err := DB.AutoMigrate(&User{}); err != nil {
+		log.Fatal("Migration failed:", err)
+	}
+}
